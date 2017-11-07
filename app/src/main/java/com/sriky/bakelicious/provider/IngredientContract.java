9bc54eede7ec1871370221d980ab2c9e65ca7a25
sriky <srikanthbm@gmail.com>
2017-11-06 17:59:21 -0800

    Refactor: make RECIPE_ID column "unique" and set conflict resolution to replace.

--- app/src/main/java/com/sriky/bakelicious/provider/IngredientContract.java
+++ app/src/main/java/com/sriky/bakelicious/provider/IngredientContract.java
@@ -20,6 +20,7 @@ import net.simonvt.schematic.annotation.ConflictResolutionType;
 import net.simonvt.schematic.annotation.DataType;
 import net.simonvt.schematic.annotation.NotNull;
 import net.simonvt.schematic.annotation.PrimaryKey;
+import net.simonvt.schematic.annotation.Unique;
 
 import static net.simonvt.schematic.annotation.DataType.Type.INTEGER;
 import static net.simonvt.schematic.annotation.DataType.Type.REAL;
@@ -33,11 +34,12 @@ import static net.simonvt.schematic.annotation.DataType.Type.TEXT;
 public interface IngredientContract {
 
     @DataType(INTEGER)
-    @PrimaryKey(onConflict = ConflictResolutionType.REPLACE)
+    @PrimaryKey
     @AutoIncrement
     String _ID = "_id";
 
     @DataType(INTEGER)
+    @Unique(onConflict = ConflictResolutionType.REPLACE)
     @NotNull
     String COLUMN_RECIPE_ID = "recipe_id";
 
--- app/src/main/java/com/sriky/bakelicious/provider/RecipeContract.java
+++ app/src/main/java/com/sriky/bakelicious/provider/RecipeContract.java
@@ -20,6 +20,7 @@ import net.simonvt.schematic.annotation.ConflictResolutionType;
 import net.simonvt.schematic.annotation.DataType;
 import net.simonvt.schematic.annotation.NotNull;
 import net.simonvt.schematic.annotation.PrimaryKey;
+import net.simonvt.schematic.annotation.Unique;
 
 import static net.simonvt.schematic.annotation.DataType.Type.INTEGER;
 import static net.simonvt.schematic.annotation.DataType.Type.TEXT;
@@ -32,11 +33,12 @@ import static net.simonvt.schematic.annotation.DataType.Type.TEXT;
 public interface RecipeContract {
 
     @DataType(INTEGER)
-    @PrimaryKey(onConflict = ConflictResolutionType.REPLACE)
+    @PrimaryKey
     @AutoIncrement
     String _ID = "_id";
 
-    @DataType(TEXT)
+    @DataType(INTEGER)
+    @Unique(onConflict = ConflictResolutionType.REPLACE)
     @NotNull
     String COLUMN_RECIPE_ID = "recipe_id";
 
